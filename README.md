0eb62e0f43a9d9f79b4b52a6d8a0720205f59781
willian <will.oliver1994@hotmail.com>
2020-05-12 11:41:58 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Zapbot
+
+Zapbot can send messages to whatsapp groups.
+
+![interface demo](demo/ZapBot_gif.gif)
+
+## Getting Started
+
+To run application, you need these tools 
+- Python;
+- Selenium;
+- ChromeDriver (version ChromeDriver 81.0.4044.20);
+
+### Prerequisites
+
+Python version: 
+
+```
+Python 3.8.2
+```
+
+Pip3 version:
+
+```
+pip 20.1 (python 3.8)
+```
+
+ChromeDriver version:
+
+You need choose the same as version that your Google Chrome version.
+
+### Installing
+
+Python:
+Link to download Python: https://www.python.org/downloads/release/python-382/
+
+During Python installation, you need choose the option Add Python to Path 
+
+ChromeDriver:
+To see your Google Chrome version, click options in your browser and select Help> About Google Chrome.
+Link to download google chrome driver : https://chromedriver.chromium.org/downloads
+
+Selenium:
+To download Selenium, in windows open Windows PowerShell and use the comand:
+
+```
+pip install selenium
+```
+
+## Usage example
+
+To start the application use the command:
+
+```
+py.exe zapbot.py
+```
---
+++ zapbot.py
@@ -0,0 +1,41 @@
+from selenium import webdriver 
+import time
+
+class WhatsappBot:
+  def __init__(self):
+    self.mensagem = "_Olá eu sou o ZapBot *Willgnis*_ :robot \n" 
+    self.mensagem2 = "_Caso você ainda não é membro, se junte ao grupo do Discord da Neo Generation: https://discord.gg/4JgZEn3_"
+
+    self.grupos = ["Neo Generation Edopro",
+    "Batalha Semanal.",
+    "Willgnis"]
+    options = webdriver.ChromeOptions()
+    options.add_argument('lang=pt-br')
+    self.driver = webdriver.Chrome(executable_path=r'./chromedriver.exe')
+
+  def EnviarMensagens(self):
+    # <div tabindex="-1" class="_1Plpp">
+    self.driver.get('https://web.whatsapp.com/')
+    time.sleep(30)
+    for grupo in self.grupos:
+        grupo = self.driver.find_element_by_xpath(f"//span[@title='{grupo}']")
+        time.sleep(3)
+        grupo.click()
+        chat_box = self.driver.find_element_by_class_name('_1Plpp')
+        time.sleep(3)
+        chat_box.click()
+        chat_box.send_keys(self.mensagem)
+        botao_enviar = self.driver.find_element_by_xpath("//span[@data-icon='send']")
+        time.sleep(3)
+        botao_enviar.click()
+        time.sleep(5)
+        chat_box.click()
+        chat_box.send_keys(self.mensagem2)
+        botao_enviar = self.driver.find_element_by_xpath("//span[@data-icon='send']")
+        time.sleep(3)
+        botao_enviar.click()
+        time.sleep(5)
+        
+
+bot = WhatsappBot()
+bot.EnviarMensagens()
